99e93544d8ea24f6708b9c54eedb3a5bb961b1da
NedaFayazi <84282492+NedaFayazi@users.noreply.github.com>
2024-10-06 13:22:14 +0330

    commit login with confilict

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'tamin-log-in',
+  selector: 'tamin-login',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.scss']
 })
